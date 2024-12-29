b514ed3b9e08296852af828f87faafb6d737dbb7
luistecnocode <luistecnocode@gmail.com>
2024-12-29 09:47:35 +0100

    Primeros operadores del 01

--- 01 - OPERADORES Y ESTRUCTURAS DE CONTROL/luistecnocode.py
+++ 01 - OPERADORES Y ESTRUCTURAS DE CONTROL/luistecnocode.py
@@ -1,25 +1,30 @@
 '''
 /*
- * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
- * - Recuerda que todas las instrucciones de participación están en el
- *   repositorio de GitHub.
+ * EJERCICIO 01 - OPERADORES Y ESTRUCTURAS DE CONTROL:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
  *
- * Lo primero... ¿Ya has elegido un lenguaje?
- * - No todos son iguales, pero sus fundamentos suelen ser comunes.
- * - Este primer reto te servirá para familiarizarte con la forma de participar
- *   enviando tus propias soluciones.
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
  *
- * EJERCICIO:
- * - Crea un comentario en el código y coloca la URL del sitio web oficial del
- *   lenguaje de programación que has seleccionado.
- * - Representa las diferentes sintaxis que existen de crear comentarios
- *   en el lenguaje (en una línea, varias...).
- * - Crea una variable (y una constante si el lenguaje lo soporta).
- * - Crea variables representando todos los tipos de datos primitivos
- *   del lenguaje (cadenas de texto, enteros, booleanos...).
- * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
- *
- * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
- * debemos comenzar por el principio.
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
  */
 '''
+
+# Operadores aritméticos
+# Utilizando fprints
+print(f"Suma: 10 + 5 = {10 + 5}")
+sum = 15 + 5
+print(f"Suma con variable: 15 + 5 = {sum}")
+print(f"Multiplicacion: 10 * 5 = {10 * 5}")
+print(f"Divison: 10 / 5 = {10 / 5}") # La division deja resultado float
+print(f"Módulo: 10 % 3 = {10 % 3}")
+print(f"Exponente: 10 ** 5 = {10 ** 5}")
+print(f"Division entera: 10 // 5 = {10 // 5}")
